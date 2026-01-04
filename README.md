e5a3b285a5f2a81971b7a4bc8bcfa53e4259ee01
shalini081996 <shalini2149@gmail.com>
2026-01-04 15:54:11 +0530

    Added problem - For Input "bbabcd" output should be "2b1a1b1c1d" and if same character appears more than 9 time than for input               "bbbbbbbbbbabcd" output should be "9b1b1a1c1d".

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # JS
 JS practice questions
 
-problem1 - Get character appearing maximum time in string "abaacda"
+problem1 - Get character appearing maximum time in string "abaacda".
+problem2 - For Input "bbabcd" output should be "2b1a1b1c1d" and if same character appears more than 9 time than for input               "bbbbbbbbbbabcd" output should be "9b1b1a1c1d".
+
+
---
+++ problem2.js
@@ -0,0 +1,24 @@
+const str = "abcdc";
+let arr = new Array(26).fill(0);
+let output = "";
+let lastCharacter;
+if (str.length == 1) {
+    output += 1 + str[0];
+} else {
+    for (let i = 0; i < str.length; i++) {
+        if (arr[str.charCodeAt(i) - "a".charCodeAt(0)] < 9) {
+            arr[str.charCodeAt(i) - "a".charCodeAt(0)]++;
+        } else {
+            output += arr[str.charCodeAt(i) - "a".charCodeAt(0)] + str[i];
+        }
+        if (lastCharacter && lastCharacter != str[i]) {
+            output += arr[str.charCodeAt(i - 1) - "a".charCodeAt(0)] + str[i - 1];
+            arr[str.charCodeAt(i - 1) - "a".charCodeAt(0)] = 0;
+        }
+        if (i == str.length - 1) {
+            output += arr[str.charCodeAt(i) - "a".charCodeAt(0)] + str[i];
+        }
+        lastCharacter = str[i];
+    }
+}
+console.log(output);
