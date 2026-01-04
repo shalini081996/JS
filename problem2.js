2c35fa62d26bf1b39ac76771cfe461a5c59ef25a
shalini081996 <shalini2149@gmail.com>
2026-01-04 17:56:53 +0530

    Added solution for problem2 without hashing.

--- problem2.js
+++ problem2.js
@@ -10,7 +10,7 @@ if (str.length == 1) {
             arr[str.charCodeAt(i) - "a".charCodeAt(0)]++;
         } else {
             output += arr[str.charCodeAt(i) - "a".charCodeAt(0)] + str[i];
-            arr[str.charCodeAt(i) - "a".charCodeAt(0)] = 0;
+            arr[str.charCodeAt(i) - "a".charCodeAt(0)] = 1;
         }
         if (lastCharacter && lastCharacter != str[i]) {
             output += arr[str.charCodeAt(i - 1) - "a".charCodeAt(0)] + str[i - 1];
@@ -23,3 +23,21 @@ if (str.length == 1) {
     }
 }
 console.log(output);
+
+// without hashing
+
+const str1 = "abbbbbbbbbbbbbbbbbcdc";
+
+let output1 = "";
+let count = 1;
+
+for (let i = 1; i <= str1.length; i++) {
+    if (str[i] === str[i - 1] && count < 9) {
+        count++;
+    } else {
+        output1 += count + str[i - 1];
+        count = 1;
+    }
+}
+
+console.log(output1);
